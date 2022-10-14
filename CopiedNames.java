d3b8467b3c0183de9784d07b5c54ee37002fdc83
diyorJAVA <101800255+diyore090@users.noreply.github.com>
2022-10-15 00:46:27 +0500

    Add files via upload

---
+++ CopiedNames.java
@@ -0,0 +1,14 @@
+package lang.print.gaps.task2;
+
+public class CopiedNames {
+    public static void main(String[] args) {
+        int variable = 0;
+        int var = 2000;
+        int value = 1000;
+        int val = 4000;
+        int threeThousand= 3000;
+        int variables = 5000;
+        int sevenThousand = 7000;
+        int ninetyThousand = 90000;
+    }
+}
---
+++ NamingConvention.java
@@ -0,0 +1,8 @@
+package lang.print.gaps.task2;
+
+public class NamingConvention {
+    public static void main(String[] args) {
+        int variable = 10;
+
+    }
+}
